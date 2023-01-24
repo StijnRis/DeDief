1eb6ed110dc7e58b88a379a8b226d680dad418de
hardekaas <77053444+hardekaas@users.noreply.github.com>
2023-01-24 19:05:35 +0100

    Weapon animations

--- DeDief/Assets/PlayerController.cs
+++ DeDief/Assets/PlayerController.cs
@@ -8,18 +8,33 @@ public class PlayerController : MonoBehaviour
     public NavMeshAgent agent;
     public GameObject target;
     public Animator animator;
+    public GameObject weapon;
+    bool aggregated;
 	
     // Start is called before the first frame update
     void Start()
     {
         agent.SetDestination(target.transform.position);
+        weapon.SetActive(false);
+        aggregated = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //animator.SetFloat("x", agent.velocity.x);
-        //animator.SetFloat("y", agent.velocity.y);
+        if (Input.GetKeyDown("space"))
+        {
+            aggregated = !aggregated;
+        }
+
+        if (aggregated)
+        {
+            weapon.SetActive(true);
+        } else
+        {
+            weapon.SetActive(false);
+        }
+        animator.SetBool("aggregated", aggregated);
         animator.SetFloat("Move", agent.velocity.magnitude);
     }
 }
