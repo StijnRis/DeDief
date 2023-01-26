c4b88d0cf3c13da99ec27e48d4dd3f8aed7420ff
Mr_Stoffer <49318416+mrstoffer@users.noreply.github.com>
2023-01-26 10:49:54 +0100

    Items with multiple sizes work now

--- DeDief/Assets/Inventory/InventoryController.cs
+++ DeDief/Assets/Inventory/InventoryController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,34 +9,93 @@ public class InventoryController : MonoBehaviour
     public ItemGrid selectedItemGrid;
 
     InventoryItem selectedItem;
+    InventoryItem overlapItem;
     RectTransform rectTransform;
 
+	[SerializeField] List<ItemData> items;
+	[SerializeField] GameObject itemPrefab;
+	[SerializeField] Transform canvasTransform;
+
     private void Update()
+    {
+        ItemIconDrag();
+
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			CreateRandomItem();
+		}
+
+        if (selectedItemGrid == null) { return; }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+			LeftMouseButtonPress();
+		}
+    }
+
+    private void CreateRandomItem()
+    {
+        InventoryItem inventoryItem = Instantiate(itemPrefab).GetComponent<InventoryItem>();
+		selectedItem = inventoryItem;
+
+		rectTransform = inventoryItem.GetComponent<RectTransform>();
+		rectTransform.SetParent(canvasTransform);
+
+		int selectedItemID = UnityEngine.Random.Range(0,items.Count);
+		inventoryItem.Set(items[selectedItemID]);
+    }
+
+    private void LeftMouseButtonPress()
+    {
+        Vector2 position = Input.mousePosition;
+
+        if (selectedItem != null)
+        {
+            position.x -= (selectedItem.itemData.width - 1) * ItemGrid.tileSizeWidth / 2;
+            position.y += (selectedItem.itemData.height - 1) * ItemGrid.tileSizeHeight / 2;
+        }
+
+        Vector2Int tileGridPosition = selectedItemGrid.GetTileGridPosition(position);
+
+        if (selectedItem == null)
+        {
+            PickUpItem(tileGridPosition);
+        }
+        else
+        {
+        	PlaceItem(tileGridPosition);
+        }
+    }
+
+    private void PlaceItem(Vector2Int tileGridPosition)
+    {
+        bool complete = selectedItemGrid.PlaceItem(selectedItem, tileGridPosition.x, tileGridPosition.y, ref overlapItem);
+        if (complete)
+        {
+            selectedItem = null;
+            if (overlapItem != null)
+            {
+                selectedItem = overlapItem;
+                overlapItem = null;
+                rectTransform = selectedItem.GetComponent<RectTransform>();
+            }
+        }  
+    }
+
+    private void PickUpItem(Vector2Int tileGridPosition)
+    {
+        selectedItem = selectedItemGrid.PickUpItem(tileGridPosition.x, tileGridPosition.y);
+        if (selectedItem != null)
+        {
+            rectTransform = selectedItem.GetComponent<RectTransform>();
+        }
+    }
+
+    private void ItemIconDrag()
     {
         if (selectedItem != null)
         {
             rectTransform.position = Input.mousePosition;
-        }
-
-        if (selectedItemGrid == null) { return; }
-
-        if (Input.GetMouseButtonDown(0)) 
-        {
-           Vector2Int tileGridPosition = selectedItemGrid.GetTileGridPosition(Input.mousePosition);
-           
-           if (selectedItem == null) 
-           {
-            selectedItem = selectedItemGrid.PickUpItem(tileGridPosition.x, tileGridPosition.y);
-            if (selectedItem != null) 
-            {
-                rectTransform = selectedItem.GetComponent<RectTransform>();
-            }
-           } 
-           else
-           {
-            selectedItemGrid.PlaceItem(selectedItem, tileGridPosition.x, tileGridPosition.y);
-            selectedItem = null;
-           }
         }
     }
 }
