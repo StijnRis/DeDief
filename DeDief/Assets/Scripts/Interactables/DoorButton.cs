5d505380eabaffca6fbb173014abe5f8fad63880
LazyGert <123165876+LazyGert@users.noreply.github.com>
2023-02-06 16:38:24 +0100

    PlayerMovement  voledig geImplementeerd
    Blijkbaar is er al ergens in een code iets met button dus moest ook dat nog veranderen

--- DeDief/Assets/Scripts/Interactables/DoorButton.cs
+++ DeDief/Assets/Scripts/Interactables/DoorButton.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Button : Interactable
+public class DoorButton : Interactable
 {
     [SerializeField]
     private GameObject door;
