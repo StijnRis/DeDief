7cce319d373650759a11b89c88de2511cc5531b7
hardekaas <77053444+hardekaas@users.noreply.github.com>
2023-02-13 16:38:56 +0100

    Storage room progress

--- DeDief/Assets/Office/Rooms/StorageRoomGenerator.cs
+++ DeDief/Assets/Office/Rooms/StorageRoomGenerator.cs
@@ -11,17 +11,68 @@ public class StorageRoomGenerator : RoomGenerator
         OnDestroy();
         PlaceWalls();
         PlaceFloor();
-        PlaceRack();
+        PlaceRacks();
         /*PlaceCeiling();*/
     }
 
-    private void PlaceRack()
+    private void PlaceRacks()
     {
-        GameObject rack = Instantiate(StorageRack, transform);
+        List<GameObject> walls = GetWalls();
 
-        BoxCollider rackSize = rack.GetComponent<BoxCollider>();
+        foreach(GameObject wall in walls)
+        {
+            if (wall.transform.rotation.y == 0) //Back wall
+            {
+                BoxCollider wallSize = wall.GetComponent<BoxCollider>();
+                Vector3 wallPosition = wall.transform.localPosition;
+                Quaternion wallRotation = wall.transform.rotation;
+
+                float wallLength = wallSize.size.z;
+
+                float rackAmount = (int) Mathf.Round(wallLength);
+
+                for (int i = 0; i < rackAmount; i++)
+                {
+                    GameObject rack = Instantiate(StorageRack, transform);
+
+                    float rackHeight = 2f;
+                    float rackDepth = 0.3f;
+                    float rackWidth = wallLength / rackAmount;
+                    float bottomPosition = wallSize.size.y / 2 - rackHeight / 2;
+
+                    BoxCollider rackSize = rack.GetComponent<BoxCollider>();
+                    Vector3 newSize = new Vector3(rackDepth, rackHeight, rackWidth);
+                    rackSize.size = newSize;
+
+                    float mostLeftPosition = -wallLength / 2 + rackWidth * 0.5f;
+                    rack.transform.localPosition = new Vector3(wallPosition.x - rackDepth * 0.5f, -bottomPosition, mostLeftPosition + rackWidth * i);
+                    rack.transform.rotation = wallRotation;
+                }
+            }
+            if (Mathf.Abs(wall.transform.rotation.y) == 90) //Side walls
+            {
+
+            }
+        }
+
+        
 
         //Place rack on outer walls
         
+    }
+
+    private List<GameObject> GetWalls()
+    {
+        List<GameObject> walls = new List<GameObject>();
+
+        foreach (Transform child in transform)
+        {
+            if (child.tag == "Wall")
+            {
+                walls.Add(child.gameObject);
+            }
+        }
+
+        return walls;
     }
 }
--- DeDief/Assets/Office/StorageRackGenerator.cs
+++ DeDief/Assets/Office/StorageRackGenerator.cs
@@ -11,8 +11,17 @@ public class StorageRackGenerator : MonoBehaviour
     {
         GameObject Rack = rackPrefabs[Random.Range(0, rackPrefabs.Length)];
         GameObject rack = Instantiate(Rack, transform);
+        BoxCollider box = rack.GetComponent<BoxCollider>();
 
-        BoxCollider roomSize = GetComponent<BoxCollider>();
-        rack.transform.localPosition = new Vector3(0, -1, 0);
+        rack.transform.localPosition = new Vector3(0, -box.size.y / 2, 0);
+        
+        Vector3 rackRotation = new Vector3(0, -90, 0);
+        rack.transform.Rotate(rackRotation);
+
+        BoxCollider maxSize = GetComponent<BoxCollider>();
+        Vector3 scale = new Vector3(maxSize.size.x / box.size.x, maxSize.size.y / box.size.y, maxSize.size.z / box.size.z);
+        rack.transform.localScale = scale;
+
+        
     }
 }
